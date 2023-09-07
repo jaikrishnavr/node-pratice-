fa4e59472299625d29188749d7642a2c212c2b87
jaikrishanvr <jaikrishnagokul@gmail.com>
2023-09-07 12:56:44 +0530

    event emitter

--- event emitter/app.js
+++ event emitter/app.js
@@ -1,5 +1,7 @@
 //note that node js also provides bunch of core modules and one of the core module is events
 // when we are using core module we dont need to provide the path for that. just the module name is enough.
+
+// Import the 'events' module in Node.js
 const events = require('events');
 
 //we can use event emitter to create events
@@ -10,10 +12,15 @@ eg:
    })
 */
 
+// Create a new event emitter object named 'myEmitter'
 const myEmitter  = new events.EventEmitter;
 
+// Register an event listener for the 'test' event
 myEmitter.on('test', function(arg){
+    // When the 'test' event is emitted, this callback function is executed,
+    // and it logs the 'arg' parameter to the console.
     console.log(arg);
 });
 
+// Emit the 'test' event with the argument 'hello world'
 myEmitter.emit('test','hello world');
